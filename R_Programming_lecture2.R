dd69ce38f14ed856d339115e28c16d93e27b7629
Chris_Rourke <chrisrourke56@gmail.com>
2015-02-08 17:03:26 -0700

    Updating files

---
+++ R_Programming_lecture2.R
@@ -0,0 +1,12 @@
+install.packages("ggplot2")
+
+source("http://bioconductor.org/biocLite.R")
+biocLite()
+biocLite(c("GenomicFeatures","AnnotationDbi"))
+
+library(ggplot2)
+search()
+
+##Installing Rtools
+
+library(KernSmooth)
---
+++ R_Programming_lecture3.R
@@ -0,0 +1,70 @@
+Descriptive Analysis
+#Just describe the population - like census
+#Not trying to make assumptions, just showing data
+
+Explatory Analysis
+#Discover new connections
+#useful for defining future studies
+#Correlation does not imply causation
+
+Inferential Analysis
+#Small sample of data say something about bigger population
+#Inference is commonly the goal of statistical models
+
+Predictive Analysis
+#use data on some objects to predict values for another object
+
+Causal Analysis
+#What happens to one variable when you make another variable change
+#Usually randomized studies
+#Causal relationships usually identified as average effects
+#Causal models usually "gold standard"
+
+Mechanistic Analysis
+#Understand exact changes in variables that lead to changes in other variables
+#hard to infer
+#Usually modeled by set of equations (phyiscal/engineering)
+
+######################
+Data are values of qualitative or quantitative variables, belonging to a set of items.
+Variables are a measurement or characteristic of an item
+
+data.gov
+
+#Data is second most important thing
+#Most important thing is the question
+#Often data will limit or enable the questions
+#having data can't save you if you don't have a quesiton
+
+######################
+Experimental Design
+#Know and care about the analysis plan
+#Have a plan for data and code sharing
+figshare.com - larger data sharing
+https://github.com/jtleek/datasharing
+
+Formulate your question in advance
+Statistical Inference -
+  #use sample,
+  #calculate descriptive statistics for the sample
+  #calculate inferential stats for the population
+
+  Age is confounder between shoe size and literacy
+    What is the other variable causing the relationship
+
+Fix a variable, stratify it, or randomize it
+
+sensitivity - p(positive test | disease)
+specificity - p(negative test | no disease)
+Positive predictive value - p(disease | positive test)
+negative predictive value - p(no disease | negative test)
+Accuracy - p(correct outcome)
+
+Good experiments
+- have replication
+- measure variability
+- generalize to the problem you care about
+- are transparent
+Prediction is not inference
+- Both can be important
+Beware data dredging
